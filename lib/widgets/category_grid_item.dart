393a7c507a77eed7c11e1990bf205793044e45c5
Risal Walangadi <risal.walangadi@gmail.com>
2024-02-16 13:29:16 +0800

    add categories screen

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meals/model/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({required this.category, super.key});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Colors.red,
+      borderRadius: const BorderRadius.all(
+        Radius.circular(16),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
